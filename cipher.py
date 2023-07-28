48b30abd8718db7d98ae6b95245a94880ce16d43
squire greene <squiregr@gmail.com>
2023-07-27 21:57:56 -0400

    Initial Commit

--- cipher.py
+++ cipher.py
@@ -1,1 +1,14 @@
-# add your code here
+abc = "abcdefghijklmnopqrstuvwxyz"
+plain_sentence = input("Please enter a sentence: ")
+plain_sentence = plain_sentence.lower
+shift = int(input("Please enter the number of places to shift "))
+
+cypher = ""
+for char in plain_sentence():
+    if char in abc:
+        index = abc.find(char)
+        index = (index + shift) % 26
+        char = abc[index]
+        cypher += char
+
+print(cypher)
